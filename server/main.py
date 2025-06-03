2b8645ffe56b1dedb48a310214b79ee7e1bc4d6e
Surya <115336926+sunkmechie@users.noreply.github.com>
2025-06-03 08:30:14 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -11,4 +11,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/", StaticFiles(directory="../client", html=True), name="static")
+import os
+print("Current working directory:", os.getcwd())
+
+client_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "client")
+print("Serving static files from:", client_dir)
+
+
+app.mount("/", StaticFiles(directory=client_dir, html=True), name="static")
+
